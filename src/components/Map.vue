e0aafc6059763bd641b52cc02a600acc970b5bfa
thomas <coreisdontgu@gmail.com>
2020-05-10 10:45:08 +0800

    update

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -9,23 +9,28 @@
 import L from "leaflet";
 console.log(L);
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-  beforeMount() {
-    console.log(document.getElementById("map"));
+  data() {
+    return {
+      data: [],
+      osmMap: {},
+      select: {
+        city: "桃園市",
+        town: "中壢區",
+      },
+    };
   },
   mounted() {
-    console.log(document.getElementById("map"));
-    const osmMap = L.map("map", {
-      center: [25.03, 121.55],
+    // console.log(document.getElementById("map"));
+    //設定地圖位子在center zoom在 15
+    this.osmMap = L.map("map", {
+      center: [24.99, 121.3],
       zoom: 15,
     });
+    //引入地圖放到id map的dom裡面
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: '<a target="_blank" href="https://www.openstreetmap.org/">© OpenStreetMap 貢獻者</a>',
       maxZoom: 18,
-    }).addTo(osmMap);
+    }).addTo(this.osmMap);
     console.log("osmap end");
   },
   created() {
@@ -39,7 +44,16 @@ export default {
     async getData() {
       console.log("get Data strat");
       await this.axios.get("https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json").then((r) => {
-        console.log(r.data.features);
+        // console.log(r.data.features);
+        this.data = r.data.features;
+      });
+      this.update();
+    },
+    update() {
+      const result = this.data.filter((e) => e.properties.county == "桃園市");
+      console.log(result);
+      result.forEach((e) => {
+        L.marker([e.geometry.coordinates[1], e.geometry.coordinates[0]]).addTo(this.osmMap);
       });
     },
   },
